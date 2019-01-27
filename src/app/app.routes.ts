1f41fa99472eda08123a24c0e948465902995f04
israel <israel.bejarano.a@gmail.com>
2019-01-27 20:44:29 +0100

    children routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,19 @@ import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
+import { PagesComponent } from './pages/pages.component'; // main route
+
 const appRoutes: Routes = [
-    {path: 'dashboard', component: DashboardComponent},
+    {   path: '',
+        component: PagesComponent,
+        children: [
+            {path: 'dashboard', component: DashboardComponent},
+            {path: 'progress', component: ProgressComponent},
+            {path: 'graficas1', component: Graficas1Component},
+            {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+         ]},
     {path: 'login', component: LoginComponent},
     {path: 'registrer', component: LoginComponent},
-    {path: 'progress', component: ProgressComponent},
-    {path: 'graficas1', component: Graficas1Component},
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: '**', component: NopagefoundComponent}
 ];
 export const APP_ROUTES = RouterModule.forRoot(appRoutes, {useHash: true});
