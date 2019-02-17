fc3d2e2939dda27c8d55be57af9c0ea38f4d6a1a
israel <israel.bejarano.a@gmail.com>
2019-02-17 22:22:14 +0100

    guard for access control

---
+++ src/app/services/guards/login-guard.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsuarioService } from '../usuario/usuario.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardGuard implements CanActivate {
+  constructor(public _usuarioService: UsuarioService, public router: Router) {}
+  canActivate() {
+    if (this._usuarioService.estaLogueado()) {
+      console.log('Paso por el Login Guard');
+      return true;
+    } else {
+      console.log('bloqueado por el Guard');
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
