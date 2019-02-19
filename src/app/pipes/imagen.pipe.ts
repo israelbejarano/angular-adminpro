200ded4e04ff60b3efb5a26590ec739c64ccda08
israel <israel.bejarano.a@gmail.com>
2019-02-19 22:40:49 +0100

    imagen pipe implementation

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { URL_SERVICIOS } from '../config/config';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return 'PIPE FUNCIONA';
+  transform(img: string, tipo: string = 'usuario'): any {
+    let url = URL_SERVICIOS + '/img';
+    if (!img) {
+      return url + '/usuario/xxx';
+    }
+
+    // imagen de google
+    if (img.indexOf('https') >= 0) {
+      return img;
+    }
+    switch (tipo) {
+      case 'usuario':
+        url += '/usuario' + img;
+      break;
+      case 'medico':
+        url += '/medico' + img;
+      break;
+      case 'hospital':
+        url += '/hospital' + img;
+      break;
+      default:
+        console.log('tipo de imagen no válido, usuario, medico, hospital');
+        url += '/usuario/xxx';
+    }
+    return url;
   }
 
 }
