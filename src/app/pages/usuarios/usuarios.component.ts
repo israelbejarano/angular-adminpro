c6aa4dc4a90031177a18628c27c199e997439642
israel <israel.bejarano.a@gmail.com>
2019-02-23 12:42:52 +0100

    change import declaration

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
-import { UsuarioService } from 'src/app/services/service.index';
+import { UsuarioService } from '../../services/service.index';
 import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
 
 declare var swal: any; // para evitar errores del IDE
