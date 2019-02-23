2f8c41039ab5187b64260bd59ec7a9720da7c1bc
israel <israel.bejarano.a@gmail.com>
2019-02-23 12:17:52 +0100

    create hospital service for CRUD

---
+++ src/app/services/hospital/hospital.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { map } from 'rxjs/operators';
+import { UsuarioService } from '../usuario/usuario.service';
+import { Hospital } from '../../models/hospital.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HospitalService {
+
+  totalHospitales = 0;
+
+  constructor(public http: HttpClient, public _usuarioService: UsuarioService) {
+    console.log('Servicio de hospital listo');
+  }
+
+  cargarHospitales() {
+    const url = URL_SERVICIOS + '/hospital';
+    return this.http.get(url).pipe(map((resp: any) => {
+      this.totalHospitales = resp.total;
+      return resp.hospital;
+    }));
+  }
+
+  obtenerHospital(id: string) {
+    const url = URL_SERVICIOS + '/hospital/' + id;
+    return this.http.get(url).pipe(map((resp: any) => {
+      return resp.hospital;
+    }));
+  }
+
+  borrarHospital(id: string) {
+    let url = URL_SERVICIOS + '/hospital/' + id;
+    url += '?token=' + this._usuarioService.token;
+    return this.http.delete(url).pipe(map((resp: any) => {
+      swal('Hospital borrado', 'El hospital ha sido borrado correctamente', 'success');
+      return true;
+    }));
+  }
+
+  crearHospital(nombre: string) {
+    let url = URL_SERVICIOS + '/hospital';
+    url += '?token=' + this._usuarioService.token;
+    return this.http.post(url, {nombre: nombre}).pipe(map((resp: any) => {
+      swal('Hospital creado', nombre, 'success');
+      return resp.hospital;
+    }));
+  }
+
+  buscarHospital(termino: string) {
+    const url = URL_SERVICIOS + '/busqueda/coleccion/hospitales/' + termino;
+    return this.http.get(url).pipe(map((resp: any) => {
+      return resp.hospitales;
+    }));
+  }
+
+  actualizarHospital(hospital: Hospital) {
+    let url = URL_SERVICIOS + '/hospital/' + hospital._id;
+    url += '?token=' + this._usuarioService.token;
+    return this.http.put(url, hosital).pipe(map((resp: any) => {
+      return resp.hospital;
+    }));
+  }
+ }
