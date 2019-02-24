5d69724d62e97a3b3c81cbe51dfd1685133584f6
israel <israel.bejarano.a@gmail.com>
2019-02-24 22:12:03 +0100

    change menu from sidebarService for load it from server response

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -15,6 +15,7 @@ export class UsuarioService {
 
   usuario: Usuario;
   token: string;
+  menu: any[] = [];
 
   constructor(public http: HttpClient, public router: Router, public _subirArchivoService: SubirArchivoService) {
     console.log('Servicio de usuario listo');
@@ -29,33 +30,39 @@ export class UsuarioService {
      if (localStorage.getItem('token')) {
        this.token = localStorage.getItem('token');
        this.usuario = JSON.parse(localStorage.getItem('usuario'));
+       this.menu = JSON.parse(localStorage.getItem('menu'));
      } else {
        this.token = '';
        this.usuario = null;
+       this.menu = [];
      }
    }
 
-   guardarStorage(id: string, token: string, usuario: Usuario) {
+   guardarStorage(id: string, token: string, usuario: Usuario, menu: any) {
     localStorage.setItem('id', id);
     localStorage.setItem('token', token);
     localStorage.setItem('usuario', JSON.stringify(usuario));
+    localStorage.setItem('menu', JSON.stringify(menu));
 
     this.usuario = usuario;
     this.token = token;
+    this.menu = menu;
    }
 
    logout() {
      this.usuario = null;
      this.token = '';
+     this.menu = [];
      localStorage.removeItem('token');
      localStorage.removeItem('usuario');
+     localStorage.removeItem('menu');
      this.router.navigate(['/login']);
    }
 
    loginGoogle(token: string) {
      const url = URL_SERVICIOS + '/login/google';
      return this.http.post(url, {token: token}).pipe(map((resp: any) => {
-      this.guardarStorage(resp.id, resp.token, resp.usuario);
+      this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu);
       return resp.usuario;
     }));
    }
@@ -69,7 +76,8 @@ export class UsuarioService {
     }
      const url = URL_SERVICIOS + '/login';
      return this.http.post(url, usuario).pipe(map((resp: any) => {
-      this.guardarStorage(resp.id, resp.token, resp.usuario);
+      this.guardarStorage(resp.id, resp.token, resp.usuario, resp.menu);
+      console.log(resp);
       return resp.usuario;
     }));
    }
@@ -88,7 +96,7 @@ export class UsuarioService {
      console.log(url);
      return this.http.put(url, usuario).pipe(map((resp: any) => { // si el usuario es el que esta logeado actualizo el storage
        if (usuario._id === this.usuario._id) {
-        this.guardarStorage(resp.usuario._id, this.token, resp.usuario);
+        this.guardarStorage(resp.usuario._id, this.token, resp.usuario, this.menu);
        }
        return resp.usuario;
      }));
@@ -99,7 +107,7 @@ export class UsuarioService {
         console.log(resp);
         this.usuario.img = resp.usuario.img;
         swal('Imagen actualizada', this.usuario.nombre, 'success');
-        this.guardarStorage(id, this.token, this.usuario);
+        this.guardarStorage(id, this.token, this.usuario, this.menu);
       });
     }
 
