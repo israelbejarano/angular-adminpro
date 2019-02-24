16448c45adf5c3d85e8eb0de1e256128ee77b5f0
israel <israel.bejarano.a@gmail.com>
2019-02-24 14:02:19 +0100

    create medico service

---
+++ src/app/services/medico/medico.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { map } from 'rxjs/operators';
+import { UsuarioService } from '../usuario/usuario.service';
+import { Medico } from '../../models/medico.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MedicoService {
+
+  totalMedicos = 0;
+  constructor(public http: HttpClient, public _usuarioService: UsuarioService) {
+    console.log('Servicio de medico listo');
+  }
+
+  cargarMedicos(desde: number) {
+    const url = URL_SERVICIOS + '/medico?desde=' + desde;
+    return this.http.get(url).pipe(map((resp: any) => {
+      this.totalMedicos = resp.total;
+      return resp.medicos;
+    }));
+  }
+
+  buscarMedicos(termino: string) {
+    const url = URL_SERVICIOS + '/busqueda/coleccion/medicos/' + termino;
+    return this.http.get(url).pipe(map((resp: any) => {
+      return resp.medicos;
+    }));
+  }
+
+  borrarMedico(id: string) {
+    let url = URL_SERVICIOS + '/medico/' + id;
+    url += '?token=' + this._usuarioService.token;
+    return this.http.delete(url).pipe(map((resp: any) => {
+      swal('Medico borrado', 'El medico ha sido borrado correctamente', 'success');
+      return true;
+    }));
+  }
+
+  guardarMedico(medico: Medico) {
+    let url = URL_SERVICIOS + '/medico/';
+    url += '?token=' + this._usuarioService.token;
+    return this.http.post(url, medico).pipe(map((resp: any) => {
+      swal('Medico creado', medico.nombre, 'success');
+      return resp.medico;
+    }));
+  }
+}
