9ccc26c0c1a34d3d6ac3e8e7af49ccba97865c6c
israel <israel.bejarano.a@gmail.com>
2019-02-27 22:15:41 +0100

    VerificaTokenGuard declaration

--- src/app/services/service.index.ts
+++ src/app/services/service.index.ts
@@ -1,9 +1,11 @@
 export { SubirArchivoService } from './subir-archivo/subir-archivo.service';
-export { LoginGuardGuard } from './guards/login-guard.guard';
-export { AdminGuard } from './guards/admin.guard';
 export { SettingsService } from './settings/settings.service';
 export { SharedService } from './shared/shared.service';
 export { SidebarService } from './shared/sidebar.service';
 export { UsuarioService } from './usuario/usuario.service';
 export { HospitalService } from './hospital/hospital.service';
 export { MedicoService } from './medico/medico.service';
+// guards
+export { LoginGuardGuard } from './guards/login-guard.guard';
+export { AdminGuard } from './guards/admin.guard';
+export { VerificaTokenGuard } from './guards/verifica-token.guard';
