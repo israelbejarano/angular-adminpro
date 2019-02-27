bdd76cb24735a72888811260a09a26761235da01
israel <israel.bejarano.a@gmail.com>
2019-02-27 22:15:26 +0100

    VerificaTokenGuard canActivated in route

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -5,7 +5,7 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccoutSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
-import { AdminGuard } from '../services/service.index';
+import { AdminGuard, VerificaTokenGuard } from '../services/service.index';
 import { ProfileComponent } from './profile/profile.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { HospitalesComponent } from './hospitales/hospitales.component';
@@ -14,7 +14,12 @@ import { MedicoComponent } from './medicos/medico.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
 
 const pagesRoutes: Routes = [
-    {path: 'dashboard', component: DashboardComponent, data: {titulo: 'Dashboard'}},
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [VerificaTokenGuard],
+        data: {titulo: 'Dashboard'}
+    },
     {path: 'progress', component: ProgressComponent, data: {titulo: 'Progress'}},
     {path: 'graficas1', component: Graficas1Component, data: {titulo: 'Gráficas'}},
     {path: 'promesas', component: PromesasComponent, data: {titulo: 'Promesas'}},
@@ -23,7 +28,12 @@ const pagesRoutes: Routes = [
     { path: 'perfil', component: ProfileComponent, data: {titulo: 'Perfil de usuario'} },
     { path: 'busqueda/:termino', component: BusquedaComponent, data: {titulo: 'Buscador'} },
     // mantenimientos
-    { path: 'usuarios', component: UsuariosComponent, canActivate: [AdminGuard], data: {titulo: 'Mantenimiento de usuarios'} },
+    {
+        path: 'usuarios',
+        component: UsuariosComponent,
+        canActivate: [AdminGuard],
+        data: {titulo: 'Mantenimiento de usuarios'}
+    },
     { path: 'hospitales', component: HospitalesComponent, data: {titulo: 'Mantenimiento de hospitales'} },
     { path: 'medicos', component: MedicosComponent, data: {titulo: 'Mantenimiento de médicos'} },
     { path: 'medico/:id', component: MedicoComponent, data: {titulo: 'Actualizar médico'} },
